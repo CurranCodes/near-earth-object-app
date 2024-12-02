1306dfef721e14cfe44ed0f7a68c7a927b610e7d
CurranTF <fitzgeraldcurran1@gmail.com>
2024-12-01 22:02:00 -0600

    Starting work on the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Button, Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,21 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable
+        style={{
+          backgroundColor: "#2c70de",
+          height: 120,
+          alignContent: "center",
+          alignItems: "center"
+        }}
+      >
+         <Text
+          style={{
+            color: "white",
+            flex: 1,
+          }}
+         >I'm pressable!</Text>
+      </Pressable>
     </View>
   );
 }
